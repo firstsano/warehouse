f33713cffc7cd06e4c3a893ba2cfcc164694935c
Koltunov Alexander <riky@vgg.ru>
2018-06-18 18:02:27 +0300

    Fix error syntax

--- spec/factories/abonents.rb
+++ spec/factories/abonents.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :abonent, class: Abonent do
-    name { |i| Faker::Name.name + " #{i}" }
-    login {  |i| Faker::Lorem.word + " #{i}" }
+    sequence(:name) { |i| Faker::Name.name + " #{i}" }
+    sequence(:login) {  |i| Faker::Lorem.word + " #{i}" }
     sequence(:bill_account)
     sequence(:profile_account)
   end
--- spec/factories/storages.rb
+++ spec/factories/storages.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :storage, class: Storage do
-    name { |i| Faker::Lorem.word + " #{i}" }
+    sequence(:name) { |i| Faker::Lorem.word + " #{i}" }
   end
 end
