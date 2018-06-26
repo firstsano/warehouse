5ee2bb0c277e4c780c0091df65a71524bdd5fbeb
Alexander Koltunov <firstsano@gmail.com>
2018-06-27 00:18:48 +0300

    Set up settings, figaro and seeds

---
+++ config/initializers/figaro.rb
@@ -0,0 +1,3 @@
+required_keys = %w[default_storage_name]
+
+Figaro.require_keys(*required_keys)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_unless_exists(active_record, attributes)
+  record_exists = active_record.where(attributes).any?
+  active_record.create! attributes unless record_exists
+end
+
+create_unless_exists Storage, name: Setting.default_storage_name
