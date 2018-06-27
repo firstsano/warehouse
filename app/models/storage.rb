982bead7edebadffa8679f47789ceb1f9b3a6263
Koltunov Alexander <riky@vgg.ru>
2018-06-27 19:09:53 +0300

    Add default_storage method

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -3,6 +3,10 @@ class Storage < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
+  def self.default_storage(settings = Setting)
+    find settings.default_storage_id
+  end
+
   def agent_name
     name
   end
--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -1,6 +1,8 @@
 require_relative 'models_helper'
 
 RSpec.describe Storage, type: :model do
+  it_should_behave_like "agent", :storage
+
   describe "validations" do
     subject { create :storage }
     it { is_expected.to validate_presence_of(:name) }
@@ -8,14 +10,20 @@ RSpec.describe Storage, type: :model do
     it_behaves_like "has simple unique validation", %i[name], :storage
   end
 
-  it_should_behave_like "agent", :storage
-
   describe "class methods" do
     subject { described_class }
+    let(:settings) { double }
 
     describe "::default_storage" do
       it { is_expected.to respond_to :default_storage }
-      its(:default_storage) { is_expected.to be_an_instance_of Storage }
+
+      it "should return an default storage from settings" do
+        storages = create_list :storage, 5
+        expected_default_storage = storages.last
+        allow(settings).to receive(:default_storage_id).and_return expected_default_storage.id
+        default_storage = subject.default_storage settings
+        expect(default_storage).to eq expected_default_storage
+      end
     end
   end
 end
